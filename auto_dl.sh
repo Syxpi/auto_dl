fc7b4b4446bf64f0bf53728fb3d756b3bc86ea20
Syxpi <syxpikiller@gmail.com>
2023-07-23 21:03:31 +0200

    Update auto_dl.sh

--- auto_dl.sh
+++ auto_dl.sh
@@ -17,3 +17,6 @@ docker pull louislam/uptime-kuma:latest
 docker run -d -p 3000:3000 --name uptime-kuma louislam/uptime-kuma:latest
 docker pull portainer/portainer-ce:latest
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+echo "Port de Uptime-Kuma: 3000"
+echo "Port de Portainer: 9443"
