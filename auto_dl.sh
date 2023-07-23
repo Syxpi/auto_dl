6ca3daa1c9ccaf9199a88c9e225b6419e2089959
Syxpi <syxpikiller@gmail.com>
2023-07-23 20:23:45 +0200

    Update auto_dl.sh

--- auto_dl.sh
+++ auto_dl.sh
@@ -1,19 +1,17 @@
-!/bin/bash
-
 apt install sudo -y
 
 sudo apt-get install neofetch -y
-
 sudo apt-get install htop -y
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --d>
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buil>
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx -y
 
 docker pull uptimekuma/uptime-kuma
 docker run -d -p 3000:3000 uptimekuma/uptime-kuma
