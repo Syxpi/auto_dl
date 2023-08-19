99c2511bed37f4d18a848f6a7fbc53d19a012a3c
Syxpi <syxpikiller@gmail.com>
2023-08-19 16:21:40 +0200

    Update auto_dl.sh

--- auto_dl.sh
+++ auto_dl.sh
@@ -8,7 +8,7 @@ apt upgrade -y
 apt install neofetch htop sudo -y
 
 # Démarrage des conteneurs Docker
-docker run -d -p 3000:3000 --name open-speedtest owenvoke/open-speedtest
+docker run -d -p 3000:3000 --name open-speedtest openspeedtest/open-speedtest
 docker run -d -p 3001:3001 --name uptime-kuma louislam/uptime-kuma
 docker run -d -p 19999:19999 --name netdata --cap-add SYS_PTRACE -v /var/run/docker.sock:/var/run/docker.sock:ro titpetric/netdata
 docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
