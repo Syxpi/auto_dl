c28854077e41864e593dde68935d41c81a6537ac
Syxpi <syxpikiller@gmail.com>
2023-08-19 16:09:56 +0200

    Update auto_dl.sh

--- auto_dl.sh
+++ auto_dl.sh
@@ -1,22 +1,14 @@
-apt install sudo -y
+#!/bin/bash
 
-sudo apt-get install neofetch -y
-sudo apt-get install htop -y
+# Mise à jour du système
+apt update
+apt upgrade -y
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker-archive-keyring.gpg
+# Installation des packages
+apt install neofetch htop sudo -y
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-docker pull louislam/uptime-kuma:latest
-docker run -d -p 3000:3000 --name uptime-kuma louislam/uptime-kuma:latest
-docker pull portainer/portainer-ce:latest
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-echo "Port de Uptime-Kuma: 3000"
-echo "Port de Portainer: 9443 (Mettez https:// au début,sinon sa fonctionne pas.)"
+# Démarrage des conteneurs Docker
+docker run -d -p 3000:3000 --name open-speedtest owenvoke/open-speedtest
+docker run -d -p 3001:3001 --name uptime-kuma louislam/uptime-kuma
+docker run -d -p 19999:19999 --name netdata --cap-add SYS_PTRACE -v /var/run/docker.sock:/var/run/docker.sock:ro titpetric/netdata
+docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
