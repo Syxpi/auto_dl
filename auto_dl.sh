9ec9398182fcf1beebe0ad0df24103f65a58ec1e
Syxpi <syxpikiller@gmail.com>
2024-03-06 16:48:49 +0100

    Update auto_dl.sh

--- auto_dl.sh
+++ auto_dl.sh
@@ -24,7 +24,7 @@ mkdir -p /jellyfin/movies musics
 docker run -d -p 3001:3001 --name uptime-kuma --restart=always louislam/uptime-kuma
 docker run -d -p 9443:9443 -p 9000:9000 -p 8000:8000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce 
 docker run -d --name=heimdall -e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -p 8006:80 -p 406:443 -v /home/heimdall:/config --restart=always lscr.io/linuxserver/heimdall:latest
-docker restart portainerocker run -d -p 9443:9443 -p 9000:9000 -p 8000:8000 --http-enabled --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker restart portainer
 cd npm
 docker compose up -d 
 echo "---------------------------------------------------------------------------------------------------------------"
@@ -34,7 +34,7 @@ echo "Ports Portainer : 9443 (HTTPS) [TCP] | 9000 (HTTP) [TCP] | 8000 (Tunnel) [
 echo "" 
 echo "Ports Heimdall : 8006 (HTTP) [TCP] | 406 (HTTPS) [TCP]"
 echo ""
-echo "Ports Nginx Proxy Manager : 80 (HTTP) [TCP/UDP] | 81 (Panel Admin HTTP) [TCP/UDP] | 443 (HTTPS) [TCP/UDP]
+echo "Ports Nginx Proxy Manager : 80 (HTTP) [TCP/UDP] | 81 (Panel Admin HTTP) [TCP/UDP] | 443 (HTTPS) [TCP/UDP]"
 echo "---------------------------------------------------------------------------------------------------------------"
 docker ps
 echo "---------------------------------------------------------------------------------------------------------------"
